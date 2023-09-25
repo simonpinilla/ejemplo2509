37271f109640cbe1430d4efdd8231f40ab43ebb9
Simon <simon.pinilla10@gmail.com>
2023-09-25 13:31:44 -0300

    vistas agregadas en la app1

--- ejemplogit/urls.py
+++ ejemplogit/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from App1 import views as v1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vista1/', v1.vista1),
+
+    
 ]
